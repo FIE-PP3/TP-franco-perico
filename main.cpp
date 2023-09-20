9013f02c8b3f6581fdfac8d1e671af8d011aa716
Franco Guarnieri <francoguarnieri@hotmail.com>
2023-09-20 11:30:05 -0300

    Cambio en el main

--- main.cpp
+++ main.cpp
@@ -1,8 +1,68 @@
+/*Programa de Gestión de Vuelos (Equipo Nro 1 - Ejercicio 3- PP3)
+------------------------------------------------------------------------
+Menú:
+1. Buscar Vuelos. // ver de agregar busqueda de reservas, que no dice la
+2. Gestionar reservas:
+3. Gestionar usuarios.
+--------------------------
+Sub-Menú (Buscar Vuelos)
+1. Por (parámetros A,B,C).
+2. Por (parámetros D,E,F).
+3. Por (parámetros ....).
+Etc...
+-----------------------------
+Sub-Menú (Gestionar reservas)
+1. Reservar
+2. Cancelar una reserva.
+---------------------------
+Sub-Menú (Gestionar usurios)
+1. Cargar nuevo usuario.
+2. Administrar Usuarios (solo Administrador)*/
+
 #include "helper.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+    menuPrincipal();
+    buscarVuelos();
+    gestionarReserva();
+    gestionarUsuario();
+}
 
 using namespace std;
 
-int main (){
-    cout<<"primer commit"<<endl;
-    return 0;
-};
+void menuPrincipal() {
+    int opcion;
+
+    while (true) {
+        cout << "Menú Principal:" << endl;
+        cout << "1. Opción 1" << endl;
+        cout << "2. Opción 2" << endl;
+        cout << "3. Opción 3" << endl;
+        cout << "4. Salir" << endl;
+        cout << "Seleccione una opción: ";
+
+        cin >> opcion;
+
+        switch (opcion) {
+            case 1:
+                cout << "Has seleccionado la Opción 1." << endl;
+                // Agrega aquí la lógica para la Opción 1
+                break;
+            case 2:
+                cout << "Has seleccionado la Opción 2." << endl;
+                // Agrega aquí la lógica para la Opción 2
+                break;
+            case 3:
+                cout << "Has seleccionado la Opción 3." << endl;
+                // Agrega aquí la lógica para la Opción 3
+                break;
+            case 4:
+                cout << "Saliendo del programa." << endl;
+                return 0;
+            default:
+                cout << "Opción no válida. Por favor, selecciona una opción válida." << endl;
+        }
+    }
+}
