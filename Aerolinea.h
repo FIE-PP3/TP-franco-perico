905189329cb85d418d21136c66bc24f8b0860f4e
lenon65 <lenonpreto65@gmail.com>
2023-09-22 20:58:14 -0300

    P1 agregue list y corregi errores varios

--- Aerolinea.h
+++ Aerolinea.h
@@ -8,10 +8,10 @@ public:
     Vuelo * buscarVuelos(struct tm fechaLlegada, struct tm fechaSalida, string origen, std::string destino);
     Reserva hacerReserva(Vuelo vuelo, Usuario usuario, int nroAsiento);
     void cancelarReservas(Reserva reservaAcancelar);
-    Reserva * listareserva;
+    list <Reserva> listareserva;
 private:    
-    Vuelo * listavuelo;
-    Usuario * listausuario;
+    list <Vuelo> listavuelo;
+    list <Usuario> listausuario;
 };
 
 #endif
