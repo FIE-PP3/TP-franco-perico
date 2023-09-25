62a1ccfe8e00f26df2cd1295ccca97d427a41a7e
lenon65 <lenonpreto65@gmail.com>
2023-09-24 23:35:10 -0300

    estado de acceso inicial + ingreso del usuario

--- Aerolinea.h
+++ Aerolinea.h
@@ -7,11 +7,25 @@ using namespace std;
 
 class Aerolinea{
 public:
-    Vuelo * buscarVuelos(struct tm fechaLlegada, struct tm fechaSalida, string origen, std::string destino);
+    void agregarvuelos(Vuelo & aux){
+        listavuelo.push_back(aux);
+    }
+    void agregarusuarios(Usuario & aux){
+        listausuario.push_back(aux);
+    }
+    bool Accesousuario(string & nombreAbuscar){
+        for (auto & aux : listausuario){
+            if ( aux.getnombre() == nombreAbuscar)
+                return true;
+        }
+        return false;
+    }
+    void imprimir (list <Vuelo> mostrar);
+    list <Vuelo> buscarVuelos(/*struct tm fechaLlegada, struct tm fechaSalida,*/ string origen, std::string destino);
     Reserva hacerReserva(Vuelo vuelo, Usuario usuario, int nroAsiento);
     void cancelarReservas(Reserva reservaAcancelar);
     list <Reserva> listareserva;
-private:    
+private:
     list <Vuelo> listavuelo;
     list <Usuario> listausuario;
 };
